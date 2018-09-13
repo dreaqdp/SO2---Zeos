a6eb74dd585b06413f64ac4009adb29e365e8746
Drea <alumne@a5s103pc34.fib.upc.es>
2018-09-13 14:20:28 +0200

    first commit

---
+++ entry.s
@@ -0,0 +1,17 @@
+# 1 "entry.S"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "entry.S"
+
+
+
+
+# 1 "include/asm.h" 1
+# 6 "entry.S" 2
+# 1 "include/segment.h" 1
+# 7 "entry.S" 2
+# 54 "entry.S"
+pushl %gs; pushl %fs; pushl %es; pushl %ds; pushl %eax; pushl %ebp; pushl %edi; pushl %esi; pushl %edx; pushl %ecx; pushl %ebx; movl $0x18, %edx; movl %edx, %ds; movl %edx, %es
